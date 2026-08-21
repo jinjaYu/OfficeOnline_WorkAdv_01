a4e71ac5ad31d055b01f7d4ae8f97433d11fe397
jinjaYu <605grac@gmail.com>
2026-08-22 01:15:56 +0800

    yap4

--- tilesets/Overworld-2.png.tsx
+++ tilesets/Overworld-2.png.tsx
@@ -1,11 +1,76 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="Overworld-2.png" tilewidth="32" tileheight="32" tilecount="72" columns="9">
  <image source="../../../../Downloads/Overworld-1.png.png" width="288" height="260"/>
- <tile id="0">
+ <tile id="27">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="28">
   <animation>
    <frame tileid="1" duration="100"/>
+   <frame tileid="4" duration="100"/>
    <frame tileid="7" duration="100"/>
    <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="14" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="45">
+  <animation>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="21" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="46">
+  <animation>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="22" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="23" duration="100"/>
   </animation>
  </tile>
 </tileset>
