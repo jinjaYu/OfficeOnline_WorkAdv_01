93baa9c5c319d4febee7b44d306328d05c376330
jinjaYu <605grac@gmail.com>
2026-08-22 10:31:41 +0800

    scascas

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ WA.onInit().then(() => {
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "I's " + time, []);
+        currentPopup = WA.ui.openPopup("clockPopup", "I'aaas " + time, []);
     })
 	
 	WA.room.area.onEnter('clock2').subscribe(() => {
@@ -26,24 +26,24 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('clock').subscribe(closePopup)
 	
 
-	  // 2. 監聽進入區域
-	  WA.room.area.onEnter('roofZone').subscribe(() => {
+	 // // 2. 監聽進入區域
+	 // WA.room.area.onEnter('roofZone').subscribe(() => {
 		
-		// 將定位物件改為 'roofZone' 確保彈出窗能正常顯示
-		WA.ui.openPopup("rz11", "It's in zone", []); 
+		//// 將定位物件改為 'roofZone' 確保彈出窗能正常顯示
+		//WA.ui.openPopup("rz11", "It's in zone", []); 
 		
-		// 顯示圖層
-		WA.room.showLayer('roof');
-	  });
+		//// 顯示圖層
+		//WA.room.showLayer('roof');
+	 // });
 
-	  // 3. 監聽離開區域
-	  WA.room.area.onLeave('roofZone').subscribe(() => {
+	 // // 3. 監聽離開區域
+	 // WA.room.area.onLeave('roofZone').subscribe(() => {
 		
-		WA.ui.openPopup("rz11", "It's out zone", []);
+		//WA.ui.openPopup("rz11", "It's out zone", []);
 		
-		// 隱藏圖層
-		WA.room.hideLayer('roof');
-	  });
+		//// 隱藏圖層
+		//WA.room.hideLayer('roof');
+	 // });
 	  
 	  WA.room.area.onEnter('point1').subscribe(() => {
 		
